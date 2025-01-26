1b5b6f7d16ee195bec42c57c61d7b6b2ccc47aa4
Fabio Monticone <monticonefabio@gmail.com>
2025-01-26 22:58:03 +0100

    added pixi ui

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,9 @@
-import { Application, ApplicationOptions } from 'pixi.js';
+import { Application, ApplicationOptions, Container } from 'pixi.js';
 import { FILL_COLOR } from './shared/constant/constants';
+
+import { Graphics } from 'pixi.js';
+
+import { Button } from '@pixi/ui';
 
 // Asynchronous IIFE
 (async () =>
@@ -24,4 +28,25 @@ import { FILL_COLOR } from './shared/constant/constants';
     // Intialize the application.
     await app.init(appOptions);
 
+    // Create a new instance of the Graphics class.
+    const graphics = new Graphics();
+    
+    //add reactangle    
+    graphics.rect(0, 0, 100, 100).fill(0xff0000);
+
+    // Add the graphics to the stage.
+
+    //add button
+    const container = new Container();
+    const button = new Button(
+        new Graphics()
+            .rect(200, 100, 100, 50)
+            .fill(0x22FFFF)
+   );
+   
+   button.onPress.connect(() => console.log('onPress'));
+   
+   container.addChild(button.view);
+    app.stage.addChild(graphics);
+    app.stage.addChild(container);
 })();
